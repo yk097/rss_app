a601d6010f9f595a43b67ac23fe396ea32cf563c
yk097 <yusukekubota97@gmail.com>
2021-06-07 07:44:18 +0900

    matometa

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,6 +1,11 @@
 class FeedsController < ApplicationController
   
   def home
+    connection = Faraday.new("http://kokoyakyumatomesokuho.blog.jp") do |builder|
+      builder.response :xml, :content_type => /\bxml$/
+    end
+    response = connection.get("/index.rdf")
+    @feeds = response.body["RDF"]["item"]
   end
   
 end
