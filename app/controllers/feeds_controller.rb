b5c1aa0d694a819bcc4782549e537ed4867f9b73
yk097 <yusukekubota97@gmail.com>
2021-06-07 23:53:50 +0900

    add col-md-4

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,9 +1,15 @@
 class FeedsController < ApplicationController
   
   def home
-    @rss1 = get_xml_response("http://kokoyakyumatomesokuho.blog.jp", "/index.rdf").body["RDF"]
-    @rss2 = get_xml_response("http://yakyu-matomeantena.blog.jp", "/index.rdf").body["RDF"]
-    @rss3 = get_xml_response("http://blog.livedoor.jp", "/nanjstu/index.rdf").body["RDF"]
+    @rss1 = get_xml_response("http://kokoyakyumatomesokuho.blog.jp", "/index.rdf")
+    @rss2 = get_xml_response("http://yakyu-matomeantena.blog.jp", "/index.rdf")
+    @rss3 = get_xml_response("http://blog.livedoor.jp", "/nanjstu/index.rdf")
+    @rss4 = get_xml_response("http://oekaki-navi.blog.jp", "/index.rdf")
+    @rss5 = get_xml_response("http://blog.livedoor.jp", "/yakiusoku/index.rdf")
+    @rss6 = get_xml_response("http://blog.livedoor.jp", "/livejupiter2/index.rdf")
+    @rss7 = get_xml_response("http://blog.livedoor.jp", "/rock1963roll/index.rdf")
+    @rss8 = get_xml_response("http://kyuukaiou.ldblog.jp", "/index.rdf")
+    @rss9 = get_xml_response("http://baseballdays.officialblog.jp", "/index.rdf")
   end
 
   
@@ -11,7 +17,7 @@ class FeedsController < ApplicationController
     connection = Faraday.new(url) do |builder|
       builder.response :xml, :content_type => /\bxml$/
     end
-    connection.get(path)
+    connection.get(path).body["RDF"]
   end
 
 end
