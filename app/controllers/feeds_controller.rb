176ecbb6c0a63d13053f9321ef5432678ab3b30c
yk097 <yusukekubota97@gmail.com>
2021-06-26 10:04:31 +0900

    searchページを追加・アウトライン作成

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,14 +4,16 @@ class FeedsController < ApplicationController
     @channels = Channel.all
   end
 
+  def search
+    @items = Channel.first.items
+  end
+
+
   # def index
   #   @search = Item.ransack(params[:q])
   #   @items = @search.result
   # end
 
-  # def search
-  #   @items = @q.result(distinct: true)
-  # end
 
 
 
